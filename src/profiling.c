490af272a1438accae2556c8e2fa1382fd8d3868
Michal Ciesielski <ciesielskimm@gmail.com>
2021-11-30 14:54:25 +0100

    Expand profiling.c

--- src/profiling.c
+++ src/profiling.c
@@ -12,3 +12,26 @@ uint32_t read_cycle_counter(void)
 {
   return DWT_CYCCNT;
 }
+
+void dwt_pcsampler_enable(void)
+{
+  // This needs to take into account the speed of the debug adapter.
+
+  // How many times the time bit must toggle before a PC sample event
+  // is generated - this acts like a prescaler.
+  DWT_CTRL &= ~DWT_CTRL_POSTPRESET;
+  DWT_CTRL |= 0x3 << DWT_CTRL_POSTPRESET_SHIFT;
+  // Choose which bit in the cycle counter is used to trigger PC sampling
+  // events - 0 to select bit 6 and 1 to select bit 10.
+  DWT_CTRL |= DWT_CTRL_CYCTAP;
+  // Enable PC sampling event.
+  DWT_CTRL |= DWT_CTRL_PCSAMPLENA;
+}
+
+void dwt_pcsampler_disable(void)
+{
+  DWT_CTRL &= ~DWT_CTRL_POSTPRESET;
+  DWT_CTRL &= ~DWT_CTRL_CYCTAP;
+  // Disable PC sampling event.
+  DWT_CTRL &= ~DWT_CTRL_PCSAMPLENA;
+}
--- src/profiling.h
+++ src/profiling.h
@@ -4,6 +4,10 @@
 #include <stdint.h>
 
 /*
+ * This is mostly inspired by https://interrupt.memfault.com/blog/profiling-firmware-on-cortex-m#poor-mans-profiler
+ */
+
+/*
  * Enable reading the PC register.
  */
 void enable_cycle_counter(void);
@@ -13,5 +17,18 @@ void enable_cycle_counter(void);
  * Remember to call enable_cycle_counter() before you do that.
  */
 uint32_t read_cycle_counter(void);
+
+/*
+ * Enable proper ITM profiling.
+ */
+void dwt_pcsampler_enable(void);
+void dwt_pcsampler_disable(void);
+
+#define PROFILE(code)         \
+  {                           \
+    dwt_pcsampler_enable();   \
+    code;                     \
+    dwt_pcsampler_disable();  \
+  }
 
 #endif // PROFILING_H
